0761bfbf9a8f3f39ac480c58dac3aa5b89a7b039
MaxAndreassenRPM <max@wolf3d.io>
2023-09-12 11:06:46 +0100

    feat: adds initial flawed test

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# qa-interview-test
+# QA Interview Test
 This repository contains the live coding test for QA candidates at Ready Player Me.
+
+## Contents
+
+In this repository you'll an find existing test with several flaws. Your tasks are the following:
+
+1. Identify the flaws in the existing test, and fix them
+2. Once provided with a new test case, write a second test to cover this test case.
+
+## Prerequisites
+
+The candidate needs to have .net 7 installed, and preferrable also Visual Studio 2022. Alternatively codesharing, screensharing and 'screen takeover' tools can be used if necessary. Be sure to ask the interviewer about this if you are having any troubles.
---
+++ selenium-test-task/ReadyPlayerMeStudioTests.cs
@@ -0,0 +1,46 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium_test_task
+{
+    public class ReadyPlayerMeStudioTests
+    {
+        private IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            driver = new ChromeDriver(path + @"\drivers\");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://studio.readyplayer.me/");
+
+            IWebElement passwordInput = driver.FindElement(By.Id("password"));
+            IWebElement emailInput = driver.FindElement(By.Id("email"));
+            IWebElement singInButton = driver.FindElement(By.XPath("//button[text()='Sign in' and @type='submit']"));
+
+            emailInput.SendKeys("qa.test.readyplayerme@gmail.com");
+            passwordInput.SendKeys("QaTest2020!");
+
+            singInButton.Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+
+            IWebElement aiCopilotButton = driver.FindElement(By.XPath("//a[text()='AI Copilot']"));
+
+            Assert.IsTrue(aiCopilotButton.Displayed);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+
+        {
+            driver.Quit();
+        }
+    }
+}
